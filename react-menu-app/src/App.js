a2a3bd26e6774aa12f1554004a4e60503360afda
carolineabbey <cmabbey22@gmail.com>
2025-02-12 01:03:37 +0000

    React Menu App - Caroline Abbey

---
+++ react-menu-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MenuApp from "./components/MenuApp";
+
+function App() {
+  return (
+    <div className="App">
+      <MenuApp />
+    </div>
+  );
+};
+
+export default App;
